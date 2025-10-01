f3b0e97aed43c8f346fd08fecc97c7c75dfbd2b0
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-10-01 16:56:20 -0300

    Add files via upload

---
+++ Calculos/logica.js
@@ -0,0 +1,16 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber 
+    var numero2 = document.getElementById("n2").valueAsNumber
+
+    var resposta = document.getElementById("respSoma")
+
+    resposta.textContent = numero1 + numero2
+
+    // console.log(numero1, numero2)
+}
+
+
+
+function sub(){
+    
+}
